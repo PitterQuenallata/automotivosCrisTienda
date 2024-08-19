9fd944323345abcdc6645898c780d7c93991ee63
PitterCasa <quenallata@gmail.com>
2024-08-19 06:57:00 -0400

    nose

--- models/conexion.php
+++ models/conexion.php
@@ -4,7 +4,7 @@ class Conexion
 {
     static public function conectar()
     {
-        $link = new PDO("mysql:host=localhost;dbname=repuestoscrispos", "root", "");
+        $link = new PDO("mysql:host=localhost;dbname=repuestoscris", "root", "");
         $link->exec("set names utf8");
         return $link;
     }
